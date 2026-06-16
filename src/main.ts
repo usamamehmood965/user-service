4c8c16843d6f6423d940c3bd3e7e4fba953a485f
Usama Mehmood <usamamehmood965@gmail.com>
2026-06-16 16:17:57 +0500

    gRPC

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,29 @@
 import { NestFactory } from '@nestjs/core';
-import { Transport } from '@nestjs/microservices';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
   // await app.listen(process.env.PORT ?? 3000);
-  const app = await NestFactory.createMicroservice(AppModule, {
-    transport: Transport.TCP,
-    options: {
-      host: '0.0.0.0',
-      port: 3001,
+  // const app = await NestFactory.createMicroservice(AppModule, {
+  //   transport: Transport.TCP,
+  //   options: {
+  //     host: '0.0.0.0',
+  //     port: 3001,
+  //   },
+  // });
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'user',
+        protoPath: join(__dirname, '../proto/user.proto'),
+        url: '0.0.0.0:50051',
+      },
     },
-  });
+  );
   await app.listen();
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,20 @@
 import { Controller } from '@nestjs/common';
-import { MessagePattern } from '@nestjs/microservices';
+import { GrpcMethod } from '@nestjs/microservices';
 import { UsersService } from './users.service';
 
 @Controller()
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @MessagePattern('get_users')
+  // @MessagePattern('get_users')
+  @GrpcMethod('UserService', 'FindAll')
   getUsers() {
-    return this.usersService.getUsers();
+    const users = this.usersService.getUsers();
+    return {
+      users: users.map((u) => ({
+        ...u,
+        id: String(u.id), // proto expects string, your mock data has number
+      })),
+    };
   }
 }
